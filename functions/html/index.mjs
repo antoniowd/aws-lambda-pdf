17e289f484fca6415d90917fb42077ac47727140
Antonio Martin <tonitim13@gmail.com>
2024-07-23 18:40:52 -0300

    generate pdf to s3

---
+++ functions/html/index.mjs
@@ -0,0 +1,41 @@
+import puppeteer from "puppeteer-core";
+import chromium from "@sparticuz/chromium";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+
+export const handler = async (event, context) => {
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath(),
+    headless: chromium.headless,
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto("https://www.example.com", { waitUntil: "networkidle0" });
+
+  const buffer = await page.pdf({ format: "A4" });
+
+  const client = new S3Client({ region: "us-east-1" });
+  const command = new PutObjectCommand({
+    Bucket: "notacerta-pdf-storage",
+    Key: "my-pdf.pdf",
+    Body: buffer,
+  });
+
+  await client.send(command);
+
+  await page.close();
+
+  await browser.close();
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      url: "https://www.example.com",
+    }),
+  };
+};
